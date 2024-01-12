7d4511ae548a5271af27b9d5b3c32a01bf444968
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2024-01-13 01:39:04 +0600

    Value change functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import Square from "./Components/Square";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNextValue, setIsXNextValue] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquare = squares.slice();
+    if (isXNextValue) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+    setSquares(nextSquare);
+    setIsXNextValue(!isXNextValue);
+  };
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-
 /* eslint-disable react/prop-types */
-const Square = () => {
-  const [value, setValue] = useState(null);
-  const handleClick = () => {
-    setValue("X");
-  };
-
+const Square = ({ value, onSquareClick }) => {
   return (
     <>
       <button
-        onClick={handleClick}
+        onClick={onSquareClick}
         className="bg-white border border-gray-600 h-24 w-24 shadow-2xl font-bold text-lg m-1 leading-9"
       >
         {value}
